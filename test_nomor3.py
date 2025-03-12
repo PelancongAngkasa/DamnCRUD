d46094f0fa1b2056350b7462adb6d070ffe773aa
PelancongAngkasa <yusufathagunawan@gmail.com>
2025-03-12 10:05:09 +0700

    modified:   .github/workflows/main.yml new file:   dockerfile modified:   test_nomor3.py new file:   wait.sh

--- test_nomor3.py
+++ test_nomor3.py
@@ -1,18 +1,23 @@
-import pytest
+import unittest, os
 from selenium import webdriver
+from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
-class TestDamnCRUD:
+class TestDamnCRUD(unittest.TestCase):
 
     @classmethod
-    @pytest.fixture(scope="class", autouse=True)
-    def setup_class(cls):
-        cls.browser = webdriver.Firefox()
-        yield
-        cls.browser.quit()
+    def setUpClass(cls):
+        option = webdriver.FirefoxOptions()
+        option.add_argument('--headless')
+        cls.browser = webdriver.Firefox(options=option)
+        try:
+            cls.url = os.environ['URL']
+        except:
+            cls.url = "http://localhost/DamnCRUD/src"
 
     def test_1_valid_creds(self):
-        self.browser.get('http://localhost/DamnCRUD/login.php')
+        self.browser.get('f"{self.url}/login.php')
         expected_result = "Howdy, damn admin!"        
         self.browser.find_element(By.ID, "inputUsername").send_keys("admin")
         self.browser.find_element(By.ID, "inputPassword").send_keys("nimda666!")
@@ -21,7 +26,7 @@ class TestDamnCRUD:
         self.assertIn(expected_result, actual_result)
 
     def test_2_invalid_username(self):           
-        self.browser.get('http://localhost/DamnCRUD/login.php')
+        self.browser.get('f"{self.url}/login.php')
         expected_result = "Damn, wrong credentials!!"
         self.browser.find_element(By.ID, "inputUsername").send_keys("invalid_user")
         self.browser.find_element(By.ID, "inputPassword").send_keys("nimda666!")
@@ -30,7 +35,7 @@ class TestDamnCRUD:
         self.assertIn(expected_result, actual_result)
         
     def test_3_invalid_password(self):
-        self.browser.get('http://localhost/DamnCRUD/login.php')           
+        self.browser.get('f"{self.url}/login.php')           
         expected_result = "Damn, wrong credentials!!"
         self.browser.find_element(By.ID, "inputUsername").send_keys("admin")
         self.browser.find_element(By.ID, "inputPassword").send_keys("invalid_password")
@@ -39,7 +44,7 @@ class TestDamnCRUD:
         self.assertIn(expected_result, actual_result)
                
     def test_4_add_contact(self):
-        self.browser.get('http://localhost/DamnCRUD/create.php')
+        self.browser.get('f"{self.url}/create.php')
         expected_result = "PelancongAngkasa"
         self.browser.find_element(By.ID, "name").send_keys("PelancongAngkasa")
         self.browser.find_element(By.ID, "email").send_keys("pelancongangkasa@email.com")
@@ -59,7 +64,7 @@ class TestDamnCRUD:
         self.assertIn(expected_result, actual_result)
     
     def test_5_sign_out(self):
-        self.browser.get('http://localhost/DamnCRUD/index.php')  
+        self.browser.get('f"{self.url}/index.php')  
         self.browser.find_element(By.XPATH, "/html/body/div[1]/div[1]/div/div/a[3]").click()
         expected_url = "http://localhost/DamnCRUD/login.php"
         actual_url = self.browser.current_url
@@ -69,3 +74,6 @@ class TestDamnCRUD:
     @classmethod
     def tearDownClass(self):
         self.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2,warnings='ignore') 
